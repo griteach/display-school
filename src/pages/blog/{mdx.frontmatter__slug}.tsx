477ab7baaebd1db8d2b3d74dd19f411e931bc9ac
griteach <griteach22@gmail.com>
2023-01-10 11:53:45 +0900

    image

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,4 +1,5 @@
 import { graphql, PageProps } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../../components/Layout";
 import Seo from "../../components/Seo";
@@ -9,8 +10,12 @@ interface IBlogPostProps {
 }
 
 export default function BlogPost({ data, children }: IBlogPostProps) {
+  const image = getImage(
+    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!
+  );
   return (
     <Layout title="">
+      <GatsbyImage image={image as any} alt={data.mdx?.frontmatter?.title!} />
       <div>{children}</div>
     </Layout>
   );
@@ -26,6 +31,11 @@ export const query = graphql`
         date
         slug
         title
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
       }
     }
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { graphql, useStaticQuery } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Author from "../components/Author";
 import Layout from "../components/Layout";
@@ -9,6 +10,10 @@ export default function IndexPage() {
     <>
       <Layout title="Main Page">
         <p>this is main page</p>
+        <StaticImage
+          src="https://images.unsplash.com/photo-1514923995763-768e52f5af87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
+          alt="sky"
+        />
       </Layout>
       <Author authorName="griteach"></Author>
     </>
